55202a191b62ada938079aca1502f011ed3efb20
devc1234 <namdevchavan713@gmail.com>
2024-07-03 14:43:05 +0530

    intial commit

---
+++ src/main/java/com/example/demo/EmployeManagmentSustemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeManagmentSustemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeManagmentSustemApplication.class, args);
+	}
+
+}
